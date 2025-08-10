0cadc2243462420f85dfdb027dee0045ba3275e2
Fabio-Valgarv <69446853+Fabio-Valgarv@users.noreply.github.com>
2025-08-10 15:16:57 -0500

    Creando el arreglo de lista de amigos
    se crea el arreglo de listaAmigos y se define la variable amigoNuevo

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+let amigoNuevo = '';
+
+function asignarTextoElemento(elemento,texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+}
+
+function agregarAmigo() {
+    let amigoNuevo = document.getElementById('amigo').value;
+    listaAmigos.push(amigoNuevo);
+    console.log(listaAmigos);
+    return;
+}
