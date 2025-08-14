2f5ebb3a61f0b4e3bfc216d7e4085f7a8287d51c
Fabio-Valgarv <69446853+Fabio-Valgarv@users.noreply.github.com>
2025-08-13 21:43:54 -0500

    Función de mostrar lista
    Se agrego la función de mostrar lista de amigos ingresados

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Declaración del arreglo y variable para agregar nombres al arreglo
 let listaAmigos = [];
 let amigoNuevo = '';
+
+
 
 function asignarTextoElemento(elemento,texto) {
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
 }
 
+//Función para agregar nombres al arreglo
 function agregarAmigo() {
     let amigoNuevo = document.getElementById('amigo').value;
-    listaAmigos.push(amigoNuevo);
-    console.log(listaAmigos);
-    return;
+    if (amigoNuevo==='') {
+        //Muestra mensaje de error si no se ingresa un nombre
+        alert('Debes ingresar un nombre!!!');
+    } else {
+        //Agrega el nombre ingresado al arreglo
+        listaAmigos.push(amigoNuevo);
+        console.log(listaAmigos);
+        mostrarLista();
+        return;
+    }
+    
 }
+
+
+//Función para mostrar los nombres ingresados
+function mostrarLista() {
+    let lista = '';
+    for (i=0;i <listaAmigos.length;i++){
+        lista += '<li>' + listaAmigos[i] + '</li>';
+    }
+    document.getElementById('listadoAmigos').innerHTML = lista;
+    
+}
+
