7ac7199a80a4d9dde638466c12261c669f413dad
Fabio-Valgarv <69446853+Fabio-Valgarv@users.noreply.github.com>
2025-08-14 21:14:55 -0500

    Finalizando el proyecto
    Se implementa la función de sortear amigo y de mostrar resultado

--- app.js
+++ app.js
@@ -3,8 +3,14 @@
 let listaAmigos = [];
 let amigoNuevo = '';
 
+// Declaración de variables para sortear amigos y arreglo de amigos sorteados
+let amigoSorteado = '';
+let amigoSecreto = '';
+let listaSorteados = [];
+let numeroSorteado = 0;
 
 
+//Asigna texto al HTML
 function asignarTextoElemento(elemento,texto) {
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
@@ -13,19 +19,26 @@ function asignarTextoElemento(elemento,texto) {
 //Función para agregar nombres al arreglo
 function agregarAmigo() {
     let amigoNuevo = document.getElementById('amigo').value;
+    document.getElementById('resultado').innerHTML = '';
     if (amigoNuevo==='') {
         //Muestra mensaje de error si no se ingresa un nombre
         alert('Debes ingresar un nombre!!!');
+        asignarTextoElemento('h2','Debes ingresar un nombre!!!');
     } else {
         //Agrega el nombre ingresado al arreglo
+        if (listaAmigos.includes(amigoNuevo)) {
+            //Muestra mensaje de error porque el nombre ingresado ya esta en la lista
+            alert('El nombre que quieres ingresar ya esta en la lista!!!');
+            asignarTextoElemento('h2','El nombre que quieres ingresar ya esta en la lista!!!');
+        } else {
         listaAmigos.push(amigoNuevo);
         console.log(listaAmigos);
         mostrarLista();
-        return;
+        asignarTextoElemento('h2','Digite el nombre de sus amigos')
+        }
+        document.querySelector('#amigo').value ='';
     }
-    
 }
-
 
 //Función para mostrar los nombres ingresados
 function mostrarLista() {
@@ -34,6 +47,44 @@ function mostrarLista() {
         lista += '<li>' + listaAmigos[i] + '</li>';
     }
     document.getElementById('listadoAmigos').innerHTML = lista;
-    
 }
 
+//Función para mostrar los nombres sorteados
+function mostrarSorteados() {
+    let lista = '';
+    for (i=0;i <listaSorteados.length;i++){
+        lista += '<li>' + 'El amigo secreto de' + ' ' + listaAmigos[i] + ' ' + 'es' + ' ' + listaSorteados[i] + '</li>';
+    }
+    document.getElementById('resultado').innerHTML = lista;
+}
+
+
+//Función para sortear amigos
+function sortearAmigo() {
+    document.getElementById('listadoAmigos').innerHTML = '';
+    if (listaAmigos.length < 3) {
+        //Muestra mensaje de error si solo son 02 amigos en la lista
+        alert('Debes ingresar un amigo más!!!');
+        asignarTextoElemento('h2','Debes ingresar un amigo más!!!');
+    } else {
+        //Sortea un amigo
+        for (i=0;i<listaAmigos.length;i){
+            amigoSorteado = listaAmigos[i];
+            numeroSorteado = Math.floor(Math.random()*listaAmigos.length);
+           //Evita que el amigo sorteado sea el mismo que el amigo secreto
+            if (i !=numeroSorteado) {
+                amigoSecreto = listaAmigos[numeroSorteado];
+                //Evita que se repita el nombre sorteado
+                if (listaSorteados.includes(amigoSecreto)) {
+                } else {
+                listaSorteados.push(amigoSecreto);
+                i++
+                }
+            }
+            mostrarSorteados();
+            console.log(listaSorteados);
+        }
+        listaAmigos = [];
+    }
+    
+}
